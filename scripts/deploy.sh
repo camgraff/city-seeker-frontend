e8f6c442d2662840cf31492390718617b33379a4
Cam <graffcameron@gmail.com>
2020-08-01 17:20:14 -0500

    Test deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,20 +3,25 @@
 # abort on errors
 set -e
 
+git config user.name "camgraff" && git config user.email "graffcameron@gmail.com"
+git checkout --orphan gh-pages
 # build
 npm run build
+git --work-tree dist add --all
+git --work-tree dist commit -m "Deploy"
+git push origin HEAD:gh-pages --force
+rm -r dist
+git checkout -f master
+git branch -D gh-pages
 
-# navigate into the build output directory
-cd dist
-
-git init
-git config user.name "camgraff" && git config user.email "graffcameron@gmail.com"
-git add -A
-git commit -m 'deploy'
+#git init
+#git config user.name "camgraff" && git config user.email "graffcameron@gmail.com"
+#git add -A
+#git commit -m 'deploy'
 
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/camgraff/city-seeker-frontend.git master:gh-pages
+## if you are deploying to https://<USERNAME>.github.io/<REPO>
+#git push -f https://github.com/camgraff/city-seeker-frontend.git master:gh-pages
 
-cd -
+#cd -
 
