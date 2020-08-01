2f1dd2d8357efeac4f578267019867a6347e6765
Cam <graffcameron@gmail.com>
2020-07-31 20:43:52 -0500

    Implemented transition from lobby to game

---
+++ src/views/Game.vue
@@ -0,0 +1,39 @@
+<template>
+    <component :is="displayComponent" :id="id" @startGame="isGameStarted = true"> </component>
+</template>
+
+<script>
+import Lobby from './Lobby.vue';
+import Play from './Play.vue';
+
+export default {
+    components: {
+        Lobby,
+        Play
+    },
+    data() {
+        return {
+            isGameStarted: false
+        };
+    },
+    computed: {
+        displayComponent() {
+            if (this.isGameStarted) {
+                return 'Play';
+            }
+            return 'Lobby';
+        }
+    },
+    props: {
+        id: {
+            type: String,
+            required: true
+        }
+    }
+    
+};
+</script>
+
+<style>
+
+</style>
