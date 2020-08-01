d24c242e02f7d96f26e6aed72fa3718b8363b724
Cam <graffcameron@gmail.com>
2020-08-01 17:07:12 -0500

    Test deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,8 +10,10 @@ npm run build
 cd dist
 
 git init
+git config user.name "camgraff" && git config user.email "graffcameron@gmail.com"
 git add -A
 git commit -m 'deploy'
+
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:camgraff/city-seeker-frontend.git master:gh-pages
