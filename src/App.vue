d722abd1b53d7e5a37718be11fff77d53d331a1a
Cam <graffcameron@gmail.com>
2020-08-03 07:03:12 -0500

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+    <div id="app">
+            <b-navbar type="dark" variant="info">
+                <b-navbar-brand to="/">City Seeker</b-navbar-brand>
+                <b-nav>
+                    <b-nav-item to="/">Play</b-nav-item>
+                </b-nav>
+            </b-navbar>
+        <router-view/>
+    </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
+nav {
+    margin-bottom: 1rem;
+    .nav-link {
+        color: darkblue;
+    }
+}
 </style>
