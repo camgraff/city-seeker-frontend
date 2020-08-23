4160eea9224c1147f073ceb17dafe0312d58639c
camgraff <graffcameron@gmail.com>
2020-08-23 16:36:54 -0500

    Make footer text smaller

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -2,13 +2,13 @@
 <footer class="border-top pt-4 mt-4">
     <div class="row">
         <div class="col">
-            <h5>Contact</h5>
+            <h6>Contact</h6>
             <ul class="list-unstyled">
                 <li><a class="text-muted" href="mailto:graffcameron@gmail.com" target="_blank">Email Us</a></li>
             </ul>
         </div>
         <div class="col">
-            <h5>Source Code</h5>
+            <h6>Source Code</h6>
             <ul class="list-unstyled">
                 <li><a class="text-muted" href="https://github.com/camgraff/city-seeker-frontend" target="_blank">Front-end</a></li>
                 <li><a class="text-muted" href="https://github.com/camgraff/city-seeker-backend" target="_blank">Back-end</a></li>
@@ -27,6 +27,7 @@ export default {
 <style scoped lang="scss">
 footer {
     text-align: center;
+    font-size: 0.75rem;
 }
 
 </style>
